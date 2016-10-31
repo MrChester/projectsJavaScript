74fffc0c8ab5c1255983430d6ceaabc4319b81f3
MrChester <iromankov2010@gmail.com>
2016-10-31 22:57:14 +0300

    commit v.1.0.69

--- lessons/#2_functions/js/main.js
+++ lessons/#2_functions/js/main.js
@@ -1,6 +1,6 @@
+// суммирование элементов
 function sum() {
     let result = 0;
-    console.log(arguments);
     for (let i = 0; i < arguments.length; i++) {
 
         if (arguments[i] instanceof Array) {
@@ -17,8 +17,27 @@ function sum() {
     }
     return result;
 }
+
+// фильтрация массива
+function filter(source, fn){
+	let result = [];
+	for(let i = 0; i<source.length; i++){
+		if(fn(source[i])){
+			result.push(source[i]);
+		}
+	}
+	return result;
+}
+
+function greaterThan4(value){
+	return value > 8;
+}
+
+
+let filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let res = filter(filterArray, greaterThan4);
 let array1 = [1, 2, 3];
 let array2 = [2, 4, 6];
 
-
-console.log(sum(array1, 5, 5, 8, '5string', array2));
+console.log(`Сумма элементов: ${sum(array1, 5, 5, 8, '5string', array2)}`);
+console.log(res);
