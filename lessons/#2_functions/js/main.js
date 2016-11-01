d48c36a5d72d6c8b5a18a27827d0851b9e1529f3
MrChester <iromankov2010@gmail.com>
2016-11-01 11:11:29 +0300

    commit v.1.0.70

--- lessons/#2_functions/js/main.js
+++ lessons/#2_functions/js/main.js
@@ -34,10 +34,27 @@ function greaterThan4(value){
 }
 
 
-let filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let res = filter(filterArray, greaterThan4);
+// замыкание
+function closures(a){
+    let b = 10;
+    function func2(){
+        let c = 100;
+        return a + b + c;
+    }
+    return func2();
+}
+
+
+// результат работы функции sum()
 let array1 = [1, 2, 3];
 let array2 = [2, 4, 6];
-
 console.log(`Сумма элементов: ${sum(array1, 5, 5, 8, '5string', array2)}`);
+
+// результат работы функции filter()
+let filterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let res = filter(filterArray, greaterThan4);
 console.log(res);
+
+// результат работы функции filter()
+let closuresRes = closures(1);
+console.log(closuresRes);
